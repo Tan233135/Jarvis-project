ea22f6eb488ced754b633e5113267a23f63973b6
Abdur Rahaman Tanvir <artanvir101@outlook.com>
2026-02-28 01:52:45 +0600

    new

--- main.py
+++ main.py
@@ -9,19 +9,34 @@ def speak(text):
     engine.say(text)
     engine.runAndWait()
 
+def processcommand(c):
+   if "open google" in c.lower():
+      webbrowser.open("https://google.com")
+   elif "open youtube" in c.lower():
+      webbrowser.open("https://youtube.com")
+   elif "open facebool" in c.lower():
+      webbrowser.open("https://facebook.com")
+   
+
 if __name__ == "__main__":
     speak("Initializing.......")
     while True:
-      r = sr.Recognizer()
-      with sr.Microphone() as source:
-         print("Listening.....")
-         audio = r.listen(source)
-
     #Recognize speech using sphinx
       try:
+         r = sr.Recognizer()
+         with sr.Microphone() as source:
+            print("Listening.....")
+            audio = r.listen(source)
+         print("Recognizing....")
          command = r.recognize_google(audio)
-         print(command) 
-      except sr.UnknownValueError:
-         print("Sphinx could not listen audio")
-      except sr.RequestError as e:
-         print("Sphinx error; {0}".format(e))
+         if(command.lower() == "jarvis"):
+            speak("Ya")
+            with sr.Microphone() as source:
+               print("jarvis Active....")
+               audio = r.listen(source)
+               command = r.recognize_google(audio)
+
+               processcommand(command)
+
+      except Exception as e:
+         print("Error; {0}".format(e))
